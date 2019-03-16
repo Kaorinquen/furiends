0a32a71c305bf47ee60ff072fac4c5cee6cb2e31
DoleTM <adylantoremember@gmail.com>
2019-03-16 11:01:44 -0400

    added routes/sequelize

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,20 @@
 var db = require("../models");
 
 module.exports = function(app) {
+  // Grabs all users from the "Users" table in the "Furiends" database
+  app.get("/api/Users", function(req, res) {
+    db.Users.findAll({}).then(function(dbUsers){
+      res.json(dbUsers);
+    });
+  });
+
+  // Adds a new user to the "Users table in the "Furiends" database
+  app.post("/api/Users", function(req, res) {
+    db.Users.create(req.body).then(function(dbUsers){
+      res.json(dbUsers);
+    });
+  });
+
   // Get all pictures from the "allPics" table in the "Furiends" database
   app.get("/api/allPics", function(req, res) {
     db.allPics.findAll({}).then(function(dbAllPics) {
@@ -8,17 +22,17 @@ module.exports = function(app) {
     });
   });
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+  // Posting a new picture to the "allPics" table in the "Furiends" database
+  app.post("/api/allPics", function(req, res) {
+    db.allPics.create(req.body).then(function(dbAllPics) {
+      res.json(dbAllPics);
     });
   });
 
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
+  // Destroying a picture in the "allPics" table in the "Furiends" database
+  app.delete("/api/allPics/:id", function(req, res) {
+    db.allPics.destroy({ where: { id: req.params.id } }).then(function(dbAllPics) {
+      res.json(dbAllPics);
     });
   });
 };

3c859d2a7f5f994c2d9b938d4c0a227f90106d73
Oscro <oscar.orchilles@gmail.com>
2019-03-20 12:20:53 -0400

    Began to add user profile route but got stuck on confirming who is accessing page

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { ensureAuthenticated } = require('../config/auth');
+const db = require('../models');
 
 // Welcome Page
 router.get('/', (req, res) => res.render('welcome'));
@@ -14,7 +15,24 @@ router.get('/dashboard', ensureAuthenticated, (req, res) =>{
 }
 );
 
-//Create User Route to separate pages and Ensure Authentication to view separate pages
+// Create User Route to separate pages and Ensure Authentication to view separate pages
 // Place condition that tests if the user is logged in
+// FEEL FREE TO CHANGE THIS
+router.get('/userprofile/:userId', ensureAuthenticated, (req, res) => {
+
+  // Add variable that holds the user accessing the page
+  // let user = ? 
+
+  // Variable that holds the profile page of the user being requested
+  let userReq = req.params.userId; 
+
+  db.User.findOne({ where: {  id: userReq } })
+        .then(user => {
+          // Condition that tests whether it is the User accessing their own page or that of another user
+          if (user){
+          res.send("USER: " + userReq);
+          }
+        }).catch(err => console.log(err));
+});
 
 module.exports = router; 

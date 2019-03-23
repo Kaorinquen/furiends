3407a759f58d9e96459a3bc74eb48e142703c4e6
The Real One <gutierrez88@gmail.com>
2019-03-23 08:40:45 -0400

    styling

--- public/js/welcome.js
+++ public/js/welcome.js
@@ -15,6 +15,23 @@ var API = {
   }
 };
 
+$(window).scroll(function() {
+  var height = $(window).scrollTop();
+  if (height > 100) {
+    $("#back2Top").fadeIn();
+  } else {
+    $("#back2Top").fadeOut();
+  }
+});
+
+$(document).ready(function() {
+  $("#back2Top").click(function(event) {
+    event.preventDefault();
+    $("html, body").animate({ scrollTop: 0 }, "slow");
+    return false;
+  });
+});
+
 API.getFront().then(function(data) {
   for (var i = data.length - 1; i >= 0; i--) {
     pictureUrls.push(data[i]);
